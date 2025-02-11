7cd0b78d12e6963e891f84f0fb583e94b97a2901
Napat6509650492 <napat.thai@dome.tu.ac.th>
2025-02-12 00:22:18 +0700

    add constructer loan

--- src/main/java/dev/napat/handytools/Model/Loan.java
+++ src/main/java/dev/napat/handytools/Model/Loan.java
@@ -5,20 +5,30 @@ import java.sql.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Loan {
-    
+
     @Id @GeneratedValue
     private Long id;
     
-    @OneToMany
+    @ManyToOne
     private Item item;
     private String borrowerName;
     private String location;
     private Date loan_date;
     private Date return_date;
+
+    Loan(){}
+
+    public Loan(Item item, String borrowerName, String location, Date loan_date, Date return_date) {
+        this.item = item;
+        this.borrowerName = borrowerName;
+        this.location = location;
+        this.loan_date = loan_date;
+        this.return_date = return_date;
+    }
     
     public Long getId() {
         return id;
